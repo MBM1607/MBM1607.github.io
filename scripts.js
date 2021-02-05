ebf119ce4fc116e6657c3aea0f8cdd9abd5b964e
mbm_1607 <muhammadkhan1607@gmail.com>
2021-02-05 18:37:10 +0500

    Make script tag defer
    Instead of loading the script before everything
    and adding an event listener on DOMContentLoaded
    simply add defer attribute to script tag. This
    allows the script to be simplified and improved.

--- scripts.js
+++ scripts.js
@@ -1,35 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const openWindowButtons = document.querySelectorAll(".open-window");
-    const closeWindowButtons = document.querySelectorAll("[data-close-button]");
-    const overlay = document.getElementById("overlay");
+const openWindowButtons = document.querySelectorAll(".open-window");
+const closeWindowButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-    openWindowButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const window = document.getElementById(button.dataset["windowTarget"]);
-            openWindow(window, overlay);
-        });
+openWindowButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const window = document.getElementById(button.dataset["windowTarget"]);
+        openWindow(window, overlay);
     });
+});
 
-    closeWindowButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const window = button.closest(".window");
-            closeWindow(window, overlay);
-        });
+closeWindowButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const window = button.closest(".window");
+        closeWindow(window, overlay);
     });
+});
 
-    overlay.addEventListener("click", () => {
-        const windows = document.querySelectorAll(".window.active");
-        windows.forEach(window => closeWindow(window, overlay));
-    })
+overlay.addEventListener("click", () => {
+    const windows = document.querySelectorAll(".window.active");
+    windows.forEach(window => closeWindow(window, overlay));
 })
 
-function openWindow(window, overlay) {
+function openWindow(window) {
     if (window == null) return;
     window.classList.add("active");
     overlay.classList.add("active");
 }
 
-function closeWindow(window, overlay) {
+function closeWindow(window) {
     if (window == null) return;
     window.classList.remove("active");
     overlay.classList.remove("active");
