dd45e0be42be27ad13b47eedf00a44dcadb44db7
mbm_1607 <muhammadkhan1607@gmail.com>
2021-02-06 18:42:12 +0500

    Make Navigation Bar responsive
    Restyled the navigation button to be tilted the
    same way as the rest of the page and added the
    tilt to projects as well. Added the on hover
    tilting to nav buttons and the project image
    buttons as well. Added a hamburger menu that is
    only to be used on smaller than 700px screens.
    The hamburger functionality works and nav bar
    is restyled with the same media query. The only
    thing left in the functionality is to animate
    the appearance of nav buttons.

---
+++ nav.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", () => {
+        if (hamburger.classList.contains("active")) {
+            hamburger.classList.remove("active");
+            navLinks.classList.remove("active");
+        }
+    })
+});
